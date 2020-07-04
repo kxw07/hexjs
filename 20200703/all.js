f8a57cc892973ab60e9f06a202d7d4db47ab7321
kai <kai@simbo.com.tw>
2020-07-04 18:10:20 +0800

    Rename tempProduct to editingProduct

--- 20200703/all.js
+++ 20200703/all.js
@@ -42,39 +42,40 @@ new Vue({
                 unit: '顆'
             }
         ],
-        tempProduct: {},
+        editingProduct: {},
         productModalIsCreating: true
     },
     methods: {
         saveProduct() {
             if (this.productModalIsCreating) {
-                this.products.push(this.tempProduct);
+                this.products.push(this.editingProduct);
             } else {
-                this.$set(this.products, this.tempProduct.index ,Object.assign({}, this.tempProduct));
+                this.$set(this.products, this.editingProduct.index ,Object.assign({}, this.editingProduct));
             }
             $('#productModal').modal('hide');
         },
         deleteProduct() {
-            this.products.splice(this.tempProduct.index, 1);
-            this.tempProduct = {};
+            this.products.splice(this.editingProduct.index, 1);
+            this.editingProduct = {};
             $('#confirmModal').modal('hide');
         },
         openModal(mode, product, index) {
             switch (mode) {
                 case 'createProduct':
                     this.productModalIsCreating = true;
-                    this.tempProduct = {};
+                    this.editingProduct = {};
+                    this.editingProduct.id = new Date().getTime();
                     $('#productModal').modal('show');
                     break;
                 case 'editProduct':
                     this.productModalIsCreating = false;
-                    this.tempProduct = Object.assign({}, product);
-                    this.tempProduct.index = index;
+                    this.editingProduct = Object.assign({}, product);
+                    this.editingProduct.index = index;
                     $('#productModal').modal('show');
                     break;
                 case 'deleteProduct':
-                    this.tempProduct = Object.assign({}, product);
-                    this.tempProduct.index = index;
+                    this.editingProduct = Object.assign({}, product);
+                    this.editingProduct.index = index;
                     $('#confirmModal').modal('show');
                     break;
                 default:
